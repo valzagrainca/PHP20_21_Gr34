f7381b617ff727dfb4737c441bc36ef8cea850be
valzagrainca <75394963+valzagrainca@users.noreply.github.com>
2021-05-20 12:13:00 +0200

    Create email.php

---
+++ fundingprograms/email.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="email.css">
+  </head>
+  <body>
+    <div class="box">
+   <form action="sendmail.php" method="POST" class="form-container">
+   <label for="emri"  style="color:#333;font-size:15px;"><b>Name</b></label>
+   </br>
+   <input type="text" id="name" placeholder="Enter Name" name="emri"  required>
+   </br>
+   <label for="email" style="color:#333;font-size:15px;"><b>Email</b></label>
+   </br>
+   <input type="text" id="email" placeholder="Enter Email" name="email"  required>
+  </br>
+   <label for="subject" style="color:#333;font-size:15px;"><b>Message</b></label>
+   </br>
+   <textarea id="subject" name="subject" placeholder="Enter text here.." cols="30" rows="4" ></textarea>
+   <button type="submit" class="btn">Summbit</button>
+   </form>
+  </div>
+                  
+  </body>
+</html>
