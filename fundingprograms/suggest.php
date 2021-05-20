b62ea7cacff54da504635d5336a683c3aea919f1
valzagrainca <75394963+valzagrainca@users.noreply.github.com>
2021-05-20 19:28:31 +0200

    Create suggest.php

---
+++ fundingprograms/suggest.php
@@ -0,0 +1,48 @@
+<?php
+$email[]="Valëza";
+$email[]="Valton";
+$email[]="Rina";
+$email[]="Riga";
+$email[]="Andi";
+$email[]="Mirjeta";
+$email[]="Bleona";
+$email[]="Tahir";
+$email[]="Doni";
+$email[]="Elona";
+$email[]="Edona";
+$email[]="Fiona";
+$email[]="Genti";
+$email[]="Hysen";
+$email[]="Lirika";
+$email[]="Lorik";
+$email[]="Vesa";
+$email[]="Lina";
+$email[]="Puhiza";
+$email[]="Bora";
+$email[]="Loris";
+$email[]="Besim";
+$email[]="Agon";
+//get query string
+$q=$_REQUEST['q'];
+$suggestion="";
+
+//get suggestions
+
+if($q!==""){
+  $q=strtolower($q);//make sure it is lowercase
+  $len=strlen($q);
+  foreach($email as $mail){
+    //finds the first occurrence of $q in mail
+    if(stristr($q,substr($mail,0,$len))){
+      if($suggestion===""){
+        $suggestion=$mail;
+      }else{
+        $suggestion .=",$mail";
+      }
+    }
+
+  }
+}
+echo $suggestion==="" ?"No Suggestion":$suggestion;
+
+?>
