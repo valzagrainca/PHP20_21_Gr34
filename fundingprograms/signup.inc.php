59c5b6340761f992652337d5a43597cbbe92dde5
rinajasharaj <75223541+rinajasharaj@users.noreply.github.com>
2021-05-21 18:53:11 +0200

    .

--- fundingprograms/signup.inc.php
+++ fundingprograms/signup.inc.php
@@ -3,12 +3,17 @@
 
 if(isset($_POST['submit'])){ //if this is set inside the code continue doing whatever u r doing
 
-    $name = mysqli_real_escape_string($conn,$_POST['name']);
-	$email = mysqli_real_escape_string($conn,$_POST['email']);
-    $username = mysqli_real_escape_string($conn,$_POST['uid']);
-    $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
-	$pwdRepeat = mysqli_real_escape_string($conn,$_POST['pwdrepeat']);
+    // $name = mysqli_real_escape_string($conn,$_POST['name']);
+	// $email = mysqli_real_escape_string($conn,$_POST['email']);
+    // $username = mysqli_real_escape_string($conn,$_POST['uid']);
+    // $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
+	// $pwdRepeat = mysqli_real_escape_string($conn,$_POST['pwdrepeat']);
 
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $username=$_POST['uid'];
+    $pwd=$_POST['pwd'];
+    $pwdRepeat=$_POST['pwdrepeat'];
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
